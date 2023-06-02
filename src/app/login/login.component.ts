567ecede10a021f5c1a0fe1633de7cfa4be8753e
Keiner Molina <keiner1027@gmail.com>
2023-06-01 20:15:34 -0500

    add library

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { startRegistration } from '@simplewebauthn/browser';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,6 @@ export class LoginComponent implements OnInit {
     });
   }
 
-
   encode(buffer: any) {
     const base64 = btoa(String.fromCharCode(...new Uint8Array(buffer)));
     return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
@@ -34,9 +34,7 @@ export class LoginComponent implements OnInit {
     return bin.buffer;
   }
 
-  async authenticate() {
-
-  }
+  async authenticate() {}
 
   form() {
     console.log(this.formLogin.value);
@@ -49,71 +47,72 @@ export class LoginComponent implements OnInit {
       )
       .subscribe((resp: any) => {
         let options = { ...resp.data };
-        console.log('1',options);
-        options.challenge = this.decode(options.challenge);
-        console.log(options);
-        options.user.id = this.decode(options.user.id);
-        console.log(options);
-        if (options.excludeCredentials) {
-          console.log(options);
+        // options.challenge = this.decode(options.challenge);
+        // options.user.id = this.decode(options.user.id);
+        // if (options.excludeCredentials) {
 
-          for (let cred of options.excludeCredentials) {
-            cred.id = this.decode(cred.id);
-          }
-          console.log(options);
+        //   for (let cred of options.excludeCredentials) {
+        //     cred.id = this.decode(cred.id);
+        //   }
 
-        }
-        console.log(options);
-        // Use platform authenticator and discoverable credential.
-        options.authenticatorSelection = {
-          authenticatorAttachment: 'platform',
-          requireResidentKey: true,
-        };
-        console.log(options);
+        // }
+        // // Use platform authenticator and discoverable credential.
+        // options.authenticatorSelection = {
+        //   authenticatorAttachment: 'platform',
+        //   requireResidentKey: true,
+        // };
 
-        navigator.credentials
-          .create({
-            publicKey: options,
-          })
-          .then(
-            (cred: any) => {
-              let credential: any = {};
-              credential.id = cred?.id as string;
-              credential.rawId = cred?.id; // Pass a Base64URL encoded ID string.
-              credential.type = cred?.type;
-              // The authenticatorAttachment string in the PublicKeyCredential object is a new addition in WebAuthn L3.
-              if (cred?.authenticatorAttachment) {
-                credential.authenticatorAttachment =
-                  cred.authenticatorAttachment;
+        // navigator.credentials
+        //   .create({
+        //     publicKey: options,
+        //   })
+        //   .then(
+        //     (cred: any) => {
+        //       let credential: any = {};
+        //       credential.id = cred?.id as string;
+        //       credential.rawId = cred?.id; // Pass a Base64URL encoded ID string.
+        //       credential.type = cred?.type;
+        //       // The authenticatorAttachment string in the PublicKeyCredential object is a new addition in WebAuthn L3.
+        //       if (cred?.authenticatorAttachment) {
+        //         credential.authenticatorAttachment =
+        //           cred.authenticatorAttachment;
+        //       }
+        //       // Base64URL encode some values.
+        //       const clientDataJSON = this.encode(cred.response.clientDataJSON);
+        //       const attestationObject = this.encode(
+        //         cred.response.attestationObject
+        //       );
+
+        //       // Obtain transports.
+        //       const transports = cred.response.getTransports
+        //         ? cred.response.getTransports()
+        //         : ['internal', 'hybrid'];
+
+        //       credential.response = {
+        //         clientDataJSON,
+        //         attestationObject,
+        //         transports,
+        //       };
+        //       console.log(credential);
+        //     },
+        //     );
+
+        startRegistration(options).then((resp) => {
+          console.log(resp);
+          this.http
+            .post(
+              'https://access-control.mango-api-dev.com/v1/verify-registration/carlos.orozco:users/carlos.orozco:current',
+              { registration: resp }
+            )
+            .subscribe(
+              (resp) => {
+                console.log(resp);
+              },
+              (err) => {
+                console.log(err);
               }
-              // Base64URL encode some values.
-              const clientDataJSON = this.encode(cred.response.clientDataJSON);
-              const attestationObject = this.encode(
-                cred.response.attestationObject
-              );
-
-              // Obtain transports.
-              const transports = cred.response.getTransports
-                ? cred.response.getTransports()
-                : ['internal', 'hybrid'];
-
-              credential.response = {
-                clientDataJSON,
-                attestationObject,
-                transports,
-              };
-              console.log(credential);
-              this.http.post(
-                'https://access-control.mango-api-dev.com/v1/verify-registration/carlos.orozco:users/carlos.orozco:current',
-                { registration:credential }
-              ).subscribe((resp)=>{
-                console.log(resp)
-              });
-            },
-            (err) => {
-              console.log(err);
-            }
-          );
+            );
+        });
       });
   }
 }
